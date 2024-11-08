d9a24e407b4da8d03be37839320e80f9175d3e57
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2024-11-08 18:59:54 -0300

    manipulando o form

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 "use strict"
 
+//CSS
 import "./styles/global.css";
 import "./styles/form.css";
-import "./styles/schedule.css"
+import "./styles/schedule.css";
+
+//Configuração do dayjs
+import "./libs/dayjs.js";
+
+import dayjs from "dayjs";
+console.log(dayjs().format("DD/mm - HH:mm"));
